db60a5abe766b5f7ad64a32e69d06180c56676db
Inna Mon <inna.mon@gmail.com>
2019-06-21 10:56:06 -0500

    added state to Board and have renderSquare method pass current state to each individual square component

--- src/index.js
+++ src/index.js
@@ -23,12 +23,22 @@ constructor(props) {
 }
 
 /* ^ arrow function makes function lexically scoped so that 'this' belongs to the Square comp and not the window */
+/* every time onClick is triggered, it calls this.setState which manipulates state data, it tells React to re-render and update this.state.value to be 'X' where ever it is clicked on the board */
+/*setState automatically updates child components */
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+
     //Board is parent of Square
     renderSquare(i) {
-        return <Square value={i}/> //value is a property of Board, pass into Sqaure so it has access to it
+        return <Square value={this.state.squares[i]} /> //value is a property of Board, pass into Sqaure so it has access to it
     }
+    /*Board instructs each individual Square about its current value ('X', 'O', or null) */
 
     render() {
         const status = 'Next player: X';
